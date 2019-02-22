12d84bbeb7777db4c4635a6bfa246a0876ec4e05
justinherold <j.herold@tcu.edu>
2019-02-21 22:46:45 -0600

    working on login and registration of admin

---
+++ src/main/java/LIC/UC04v1/model/Role.java
@@ -0,0 +1,17 @@
+package LIC.UC04v1.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+}
---
+++ src/main/java/LIC/UC04v1/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package LIC.UC04v1.repositories;
+
+import LIC.UC04v1.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
