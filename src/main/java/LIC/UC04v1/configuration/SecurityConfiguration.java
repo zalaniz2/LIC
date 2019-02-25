2d1f95e1d0051beb9d1067de44f1ad06c76ce9a1
justinherold <j.herold@tcu.edu>
2019-02-25 13:27:20 -0600

    now only admins can access registration page

--- src/main/java/LIC/UC04v1/configuration/SecurityConfiguration.java
+++ src/main/java/LIC/UC04v1/configuration/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/export").permitAll()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/registration").permitAll()
+                //.antMatchers("/registration").hasAuthority("ADMIN")
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
