e4fdeb6622e2b6210bf5c6f90f71a07ccbef4d6d
ABParris <a.b.parris@tcu.edu>
2019-02-28 16:17:08 -0600

    comment Locaion

--- src/main/java/LIC/UC04v1/controllers/Location.java
+++ src/main/java/LIC/UC04v1/controllers/Location.java
@@ -1,5 +1,8 @@
 package LIC.UC04v1.controllers;
-
+/*
+    This enum is where the areas where the general LIC clerkship areas are stored and pulled from.
+    Altering this list will change the locations available across the board.
+ */
 public enum Location {
     FortWorth("Fort Worth"), Denton("Denton"), Dallas("Dallas"), KellerSouthLakeAlliance("Keller/South Lake/Alliance"), Arlington("Arlington"), Mansfield("Mansfield");
     private final String name;
