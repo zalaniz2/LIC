24e20a97eb7160ebf41773761e44c5ec9535fa79
kortstadt <katie.ortstadt@tcu.edu>
2019-03-30 18:52:36 -0500

    Update the database for Phase 1

---
+++ src/main/java/LIC/UC04v1/repositories/AdminRepository.java
@@ -0,0 +1,7 @@
+package LIC.UC04v1.repositories;
+
+import LIC.UC04v1.model.Admin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdminRepository extends CrudRepository<Admin, Integer> {
+}
